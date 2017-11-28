fe7a404f41f21a77705b55421ff76968c88393f5
kkv263 <kkv263@utexas.edu>
2017-11-28 17:54:29 -0600

    homepage progress

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Homepage from './js/Homepage';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      <h1>Hello World!</h1>
-
+      <div>
+        <Homepage/>
       </div>
     );
   }
---
+++ src/js/Homepage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { HomeWrapper } from '../styles/Homepage.style'
+
+class Homepage extends Component {
+  render() {
+    return (
+      <HomeWrapper> 
+        hello
+      </HomeWrapper> 
+    );
+  }
+}
+
+export default Homepage;
