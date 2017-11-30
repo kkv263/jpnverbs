5058f6a1eadc9dc1cc199b5fb5e26f7597018352
kkv263 <kkv263@utexas.edu>
2017-11-30 15:47:27 -0600

    routes configured

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import Homepage from './js/Homepage';
+import NotFound from './js/NotFound';
+import { Switch, Route } from 'react-router-dom';
+
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Homepage/>
+        <Switch>
+         <Route exact path='/' component={Homepage}/>
+         <Route component={NotFound} status={404}/>
+        </Switch>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import {AppContainer} from 'react-hot-loader';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<BrowserRouter><App /></BrowserRouter>,document.getElementById('root'));
 registerServiceWorker();
 
 if (module.hot) {
